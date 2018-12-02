60118ad142aba9a824b8ea88915214dc84ea8bd1
Ivo Woltring <Ivo@IvoNet.nl>
2018-12-02 22:22:23 +0100

    initial config

--- src/main/resources/archetype-resources/run.sh
+++ src/main/resources/archetype-resources/run.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
-rm -rfv ./artifact/*.war
-mvn clean package
-RUNNING=$(docker inspect --format="{{ .State.Running }}" ${artifactId} 2> /dev/null)
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+#!/usr/bin/env bash
+
+RUNNING=$(docker inspect --format="{{ .State.Running }}" jee 2> /dev/null)
 if [ $? -eq 1 ] || [ "$RUNNING" == "false" ]; then
+    rm -rfv ./artifact/*.war
+
     docker run \
      -d \
      --rm \
-     --name ${artifactId} \
+     --name jee \
      -p 8080:8080 \
      -p 9990:9990 \
      -v "$(pwd)/artifact:/deployments" \
@@ -16,8 +20,9 @@ if [ $? -eq 1 ] || [ "$RUNNING" == "false" ]; then
     echo "Usr: admin"
     echo "Pwd: s3cr3t"
 
+    mvn clean package
 else
-    /usr/bin/osascript -e 'display notification "Stopping..." with title "${artifactId}"'
-    docker rm -f ${artifactId}
+    /usr/bin/osascript -e 'display notification "Stopping..." with title "jee"'
+    docker rm -f jee
 fi
 
--- src/main/resources/archetype-resources/src/main/java/service/package-info.java
+++ src/main/resources/archetype-resources/src/main/java/service/package-info.java
@@ -1,5 +1,9 @@
 #set( $symbol_pound = '#' )
 #set( $symbol_dollar = '$' )
 #set( $symbol_escape = '\' )
-#!/usr/bin/env bash
-docker logs ${artifactId} -f
+
+
+/**
+ * Restful services here
+ */
+package ${package}.service;
